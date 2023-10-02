bd205f5c8204d8e1550bfb77ea1889edf16335f5
Yoo Seungmin <dnjfht@gmail.com>
2023-10-02 11:38:59 +0900

    dark mode css 변경

--- .history/src/app/Providers_20231002112355.jsx
+++ .history/src/app/Providers_20231002112355.jsx
@@ -5,9 +5,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({ children }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-color duration-300 min-h-screen select-nonr">
-        {children}
-      </div>
+      <div>{children}</div>
     </ThemeProvider>
   );
 }
